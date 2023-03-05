9b4af1b0c928bd0e65be6f0ab8398b9751078af8
Evelina Pancheva <81183303+evelinagp@users.noreply.github.com>
2023-03-05 12:48:31 +0200

    Add files via upload

---
+++ src/FinalExam/Pr2.java
@@ -0,0 +1,32 @@
+package FinalExam;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Pr2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        String regex = "^(\\$|%)(?<tag>[A-Z][a-z]{3,})\\1:\\s(\\[(?<number1>[0-9]+)]\\|\\[(?<number2>[0-9]+)]\\|\\[(?<number3>[0-9]+)]\\|){1,3}$";
+        Pattern pattern = Pattern.compile(regex);
+        for (int i = 0; i < n; i++) {
+            String input = scanner.nextLine();
+            Matcher matcher = pattern.matcher(input);
+            StringBuilder sb = new StringBuilder();
+                if (matcher.find()){
+                    String tag = matcher.group("tag");
+                    char number1 = (char)Integer.parseInt(matcher.group("number1"));
+                    char number2 = (char)Integer.parseInt(matcher.group("number2"));
+                    char number3 = (char)Integer.parseInt(matcher.group("number3"));
+                    sb.append(number1);
+                    sb.append(number2);
+                    sb.append(number3);
+                    System.out.println(tag + ": "+ sb.toString());
+                }else{
+                    System.out.println("Valid message not found!");
+                }
+
+        }
+    }
+}
---
+++ src/MidExam/Pr2.java
@@ -0,0 +1,54 @@
+package MidExam;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Pr2 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<String> list = Arrays.stream(scanner.nextLine().split(", ")).collect(Collectors.toList());
+
+        int lostCounter = 0;
+        int blacklistedCounter = 0;
+        String input = scanner.nextLine();
+        while (!input.equals("Report")) {
+            String[] tokens = input.split("\\s+");
+            String command = tokens[0];
+
+            if (command.equals("Blacklist")) {
+                String name = tokens[1];
+                if (list.contains(name)) {
+                    System.out.printf("%s was blacklisted.%n", name);
+                    list.set(list.indexOf(name), "Blacklisted");
+                    blacklistedCounter++;
+                } else {
+                    System.out.printf("%s was not found.%n", name);
+                }
+            } else if (command.equals("Error")) {
+                int index = Integer.parseInt(tokens[1]);
+                if (!((list.get(index).equals("Blacklisted")) || (list.get(index).equals("Lost")))) {
+                    System.out.printf("%s was lost due to an error.%n", list.get(index));
+                    list.set(index, "Lost");
+                    lostCounter++;
+                }
+            } else if (command.equals("Change")) {
+                int index = Integer.parseInt(tokens[1]);
+                String newName = tokens[2];
+                if (index >= 0 && index < list.size()) {
+                    System.out.printf("%s changed his username to %s.%n", list.get(index), newName);
+                    list.set(index, newName);
+                }
+            }
+            input = scanner.nextLine();
+        }
+        System.out.printf("Blacklisted names: %d%n", blacklistedCounter);
+        System.out.printf("Lost names: %d%n", lostCounter);
+        for (String name : list) {
+            System.out.print(name + " ");
+        }
+    }
+}
